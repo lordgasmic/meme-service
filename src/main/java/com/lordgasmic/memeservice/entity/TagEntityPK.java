c938e6ae1a57dcc1549eeaffada20be52163121b
Nick Wellman <wellman.nicholas@gmail.com>
2021-03-29 14:50:30 -0400

    update entity pk

--- src/main/java/com/lordgasmic/memeservice/entity/TagEntityPK.java
+++ src/main/java/com/lordgasmic/memeservice/entity/TagEntityPK.java
@@ -5,12 +5,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
+import java.io.Serializable;
 
 @Embeddable
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class TagEntityPK {
+public class TagEntityPK implements Serializable {
 
     private String id;
     private String uuid;
