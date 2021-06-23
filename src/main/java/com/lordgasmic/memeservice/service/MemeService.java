d707af3957a427a2b8afb356109ba720c23b4da7
Nick Wellman <wellman.nicholas@gmail.com>
2021-06-23 00:35:42 -0400

    attempt to log

--- src/main/java/com/lordgasmic/memeservice/service/MemeService.java
+++ src/main/java/com/lordgasmic/memeservice/service/MemeService.java
@@ -69,13 +69,14 @@ public class MemeService {
         Map<String, String> queryParamMap = new HashMap<>();
         queryParamMap.put("q", "tag:" + tag);
         QueryResponse solrResponse = solrClient.query(new MapSolrParams(queryParamMap));
-        List<String> tagIds = solrResponse.getBeans(Doc.class).stream().map(Doc::getId).collect(toList());
-        List<MemeEntity> memes = memeRepository.findByIdIn(tagIds);
-        List<String> memeIds = memes.stream().map(MemeEntity::getId).collect(toList());
+        log.info(solrResponse.getResults().toString());
+        //        List<String> tagIds = solrResponse.getgetBeans(Doc.class).stream().map(Doc::getId).collect(toList());
+        //        List<MemeEntity> memes = memeRepository.findByIdIn(tagIds);
+        //        List<String> memeIds = memes.stream().map(MemeEntity::getId).collect(toList());
 
         List<MemeResponse> response = new ArrayList<>();
 
-        getMemeAttributesAndAssociate(memeIds, response);
+        //        getMemeAttributesAndAssociate(memeIds, response);
 
         return response;
     }
