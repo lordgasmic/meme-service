064d3463a045d2af55960318c10d2bf5973b34c9
Nick Wellman <wellman.nicholas@gmail.com>
2022-09-01 14:16:40 -0400

    save action

--- src/main/java/com/lordgasmic/memeservice/service/MemeService.java
+++ src/main/java/com/lordgasmic/memeservice/service/MemeService.java
@@ -28,7 +28,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static java.util.stream.Collectors.*;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 
 @Service
 @Slf4j
@@ -46,7 +48,7 @@ public class MemeService {
     @Autowired
     private RequestRepository requestRepository;
 
-    private HttpSolrClient solrClient;
+    private final HttpSolrClient solrClient;
 
     public MemeService() {
         solrClient = new HttpSolrClient.Builder("http://solr:8983/solr/memes").build();
@@ -55,56 +57,56 @@ public class MemeService {
     private static final Gson gson = new Gson();
 
     public List<MemeResponse> getAllMemes() {
-        List<MemeEntity> memes = memeRepository.findAll();
-        List<String> memeIds = memes.stream().map(MemeEntity::getId).collect(toList());
+        final List<MemeEntity> memes = memeRepository.findAll();
+        final List<String> memeIds = memes.stream().map(MemeEntity::getId).collect(toList());
 
-        List<MemeResponse> response = new ArrayList<>();
+        final List<MemeResponse> response = new ArrayList<>();
 
         getMemeAttributesAndAssociate(memeIds, response);
 
         return response;
     }
 
-    public List<MemeResponse> getMemesByTag(String tag) throws IOException, SolrServerException {
-        Map<String, String> queryParamMap = new HashMap<>();
+    public List<MemeResponse> getMemesByTag(final String tag) throws IOException, SolrServerException {
+        final Map<String, String> queryParamMap = new HashMap<>();
         queryParamMap.put("q", "tag:" + tag);
-        QueryResponse solrResponse = solrClient.query(new MapSolrParams(queryParamMap));
+        final QueryResponse solrResponse = solrClient.query(new MapSolrParams(queryParamMap));
 
-        List<String> tagIds = solrResponse.getResults().stream().map(d -> (String) d.get("id")).collect(toList());
-        List<MemeEntity> memes = memeRepository.findByIdIn(tagIds);
-        List<String> memeIds = memes.stream().map(MemeEntity::getId).collect(toList());
+        final List<String> tagIds = solrResponse.getResults().stream().map(d -> (String) d.get("id")).collect(toList());
+        final List<MemeEntity> memes = memeRepository.findByIdIn(tagIds);
+        final List<String> memeIds = memes.stream().map(MemeEntity::getId).collect(toList());
 
-        List<MemeResponse> response = new ArrayList<>();
+        final List<MemeResponse> response = new ArrayList<>();
 
         getMemeAttributesAndAssociate(memeIds, response);
 
         return response;
     }
 
-    public void addMeme(MultipartFile file) throws IOException {
-        File f = new File("/vol/" + file.getOriginalFilename());
+    public void addMeme(final MultipartFile file) throws IOException {
+        final File f = new File("/vol/" + file.getOriginalFilename());
         file.transferTo(f);
     }
 
-    public void addMemeRequest(MemeRequestRequest request) {
+    public void addMemeRequest(final MemeRequestRequest request) {
         log.info("request = " + request);
-        RequestEntity entity = new RequestEntity();
+        final RequestEntity entity = new RequestEntity();
         entity.setName(request.getName());
         requestRepository.save(entity);
     }
 
     public void updateIndex() throws IOException, SolrServerException {
-        List<TagEntity> tags = tagRepository.findAll();
+        final List<TagEntity> tags = tagRepository.findAll();
 
-        Map<String, List<String>> tagMap = new HashMap<>();
-        for (TagEntity tag : tags) {
+        final Map<String, List<String>> tagMap = new HashMap<>();
+        for (final TagEntity tag : tags) {
             tagMap.computeIfAbsent(tag.getPk().getId(), k -> new ArrayList<>());
 
             tagMap.get(tag.getPk().getId()).add(tag.getTag());
         }
 
-        List<Doc> docs = new ArrayList<>();
-        for (String key : tagMap.keySet()) {
+        final List<Doc> docs = new ArrayList<>();
+        for (final String key : tagMap.keySet()) {
             docs.add(new Doc(key, tagMap.get(key)));
         }
 
@@ -115,14 +117,14 @@ public class MemeService {
         solrClient.commit();
     }
 
-    private void getMemeAttributesAndAssociate(List<String> memeIds, List<MemeResponse> response) {
-        List<TagEntity> tags = tagRepository.findByPkIdIn(memeIds);
-        List<PathEntity> paths = pathRepository.findAllByIdIn(memeIds);
-        Map<String, List<TagEntity>> tagMap = tags.stream().collect(groupingBy(tag -> tag.getPk().getId()));
-        Map<String, String> pathMap = paths.stream().collect(toMap(PathEntity::getId, PathEntity::getPath));
+    private void getMemeAttributesAndAssociate(final List<String> memeIds, final List<MemeResponse> response) {
+        final List<TagEntity> tags = tagRepository.findByPkIdIn(memeIds);
+        final List<PathEntity> paths = pathRepository.findAllByIdIn(memeIds);
+        final Map<String, List<TagEntity>> tagMap = tags.stream().collect(groupingBy(tag -> tag.getPk().getId()));
+        final Map<String, String> pathMap = paths.stream().collect(toMap(PathEntity::getId, PathEntity::getPath));
 
-        for (String s : memeIds) {
-            MemeResponse meme = new MemeResponse();
+        for (final String s : memeIds) {
+            final MemeResponse meme = new MemeResponse();
             meme.setName(s);
             meme.setTags(tagMap.get(s).stream().map(TagEntity::getTag).collect(toList()));
             meme.setUrl(pathMap.get(s));
