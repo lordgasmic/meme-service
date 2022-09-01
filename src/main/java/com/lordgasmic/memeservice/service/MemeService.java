0cf66321ee48fee0e24942ac30542b30c72b95a0
Nick Wellman <wellman.nicholas@gmail.com>
2022-09-01 14:58:59 -0400

    remove debug

--- src/main/java/com/lordgasmic/memeservice/service/MemeService.java
+++ src/main/java/com/lordgasmic/memeservice/service/MemeService.java
@@ -130,8 +130,7 @@ public class MemeService {
             meme.setTags(tagMap.get(s).stream().map(TagEntity::getTag).collect(toList()));
             meme.setUrl(pathMap.get(s));
             meme.setThumbnailUrl(thumbnailPathMap.get(s));
-            log.info("meme: {}", meme);
-            
+
             response.add(meme);
         }
     }
