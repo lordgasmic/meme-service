10633c4c75da042a8fc86e7bfec3996d52b8c0eb
Nick Wellman <wellman.nicholas@gmail.com>
2022-09-01 14:05:10 -0400

    add insert for more images

---
+++ db-meme/sql/v1.202208241157__Insert.sql
@@ -0,0 +1,287 @@
+insert into meme (id)
+values ('573a04b.jpg');
+insert into meme_tag (id, uuid, tag)
+values ('573a04b.jpg', UUID(), 'art');
+insert into meme_tag (id, uuid, tag)
+values ('573a04b.jpg', UUID(), 'star wars');
+insert into meme_tag (id, uuid, tag)
+values ('573a04b.jpg', UUID(), 'x wing');
+insert into meme_path (id, path)
+values ('573a04b.jpg', '/images/memes/573a04b.jpg');
+
+insert into meme (id)
+values ('bael.jpg');
+insert into meme_tag (id, uuid, tag)
+values ('bael.jpg', UUID(), 'art');
+insert into meme_tag (id, uuid, tag)
+values ('bael.jpg', UUID(), 'dwarf');
+insert into meme_tag (id, uuid, tag)
+values ('bael.jpg', UUID(), 'dnd');
+insert into meme_path (id, path)
+values ('bael.jpg', '/images/memes/bael.jpg');
+
+insert into meme (id)
+values ('46f.gif');
+insert into meme_tag (id, uuid, tag)
+values ('46f.gif', UUID(), 'vince mcmahon');
+insert into meme_tag (id, uuid, tag)
+values ('46f.gif', UUID(), 'you\'re fired');
+insert into meme_tag (id, uuid, tag)
+values ('46f.gif', UUID(), 'wwe');
+insert into meme_tag (id, uuid, tag)
+values ('46f.gif', UUID(), 'wwf');
+insert into meme_path (id, path)
+values ('46f.gif', '/images/memes/46f.gif');
+
+insert into meme (id)
+values ('1465591290350.gif');
+insert into meme_tag (id, uuid, tag)
+values ('1465591290350.gif', UUID(), 'dean winchester');
+insert into meme_tag (id, uuid, tag)
+values ('1465591290350.gif', UUID(), 'supernatural');
+insert into meme_tag (id, uuid, tag)
+values ('1465591290350.gif', UUID(), 'thumbs up');
+insert into meme_path (id, path)
+values ('1465591290350.gif', '/images/memes/1465591290350.gif');
+
+insert into meme (id)
+values ('1523887944308.gif');
+insert into meme_tag (id, uuid, tag)
+values ('1523887944308.gif', UUID(), 'south park');
+insert into meme_tag (id, uuid, tag)
+values ('1523887944308.gif', UUID(), 'nice');
+insert into meme_path (id, path)
+values ('1523887944308.gif', '/images/memes/1523887944308.gif');
+
+insert into meme (id)
+values ('a1c677c7503489df0a741d4d86596d22.gif');
+insert into meme_tag (id, uuid, tag)
+values ('a1c677c7503489df0a741d4d86596d22.gif', UUID(), 'frozen');
+insert into meme_tag (id, uuid, tag)
+values ('a1c677c7503489df0a741d4d86596d22.gif', UUID(), 'anna');
+insert into meme_tag (id, uuid, tag)
+values ('a1c677c7503489df0a741d4d86596d22.gif', UUID(), 'tick tok');
+insert into meme_path (id, path)
+values ('a1c677c7503489df0a741d4d86596d22.gif', '/images/memes/a1c677c7503489df0a741d4d86596d22.gif');
+
+insert into meme (id)
+values ('bloLRQO.gif');
+insert into meme_tag (id, uuid, tag)
+values ('bloLRQO.gif', UUID(), 'archer');
+insert into meme_tag (id, uuid, tag)
+values ('bloLRQO.gif', UUID(), 'margarita');
+insert into meme_tag (id, uuid, tag)
+values ('bloLRQO.gif', UUID(), 'ingredients');
+insert into meme_path (id, path)
+values ('bloLRQO.gif', '/images/memes/bloLRQO.gif');
+
+insert into meme (id)
+values ('boom.gif');
+insert into meme_tag (id, uuid, tag)
+values ('boom.gif', UUID(), 'purepwnage');
+insert into meme_tag (id, uuid, tag)
+values ('boom.gif', UUID(), 'fps dave');
+insert into meme_tag (id, uuid, tag)
+values ('boom.gif', UUID(), 'boom head shot');
+insert into meme_tag (id, uuid, tag)
+values ('boom.gif', UUID(), 'counter strike');
+insert into meme_path (id, path)
+values ('boom.gif', '/images/memes/boom.gif');
+
+insert into meme (id)
+values ('boo-this-man.gif');
+insert into meme_tag (id, uuid, tag)
+values ('boo-this-man.gif', UUID(), 'boo this man');
+insert into meme_path (id, path)
+values ('boo-this-man.gif', '/images/memes/boo-this-man.gif');
+
+insert into meme (id)
+values ('darn-thats-awesome.gif');
+insert into meme_tag (id, uuid, tag)
+values ('darn-thats-awesome.gif', UUID(), 'metalocalypse');
+insert into meme_tag (id, uuid, tag)
+values ('darn-thats-awesome.gif', UUID(), 'nathan explosion');
+insert into meme_tag (id, uuid, tag)
+values ('darn-thats-awesome.gif', UUID(), 'darn that\'s awesome');
+insert into meme_path (id, path)
+values ('darn-thats-awesome.gif', '/images/memes/darn-thats-awesome.gif');
+
+insert into meme (id)
+values ('gay-fish.gif');
+insert into meme_tag (id, uuid, tag)
+values ('gay-fish.gif', UUID(), 'south park');
+insert into meme_tag (id, uuid, tag)
+values ('gay-fish.gif', UUID(), 'gay fish');
+insert into meme_path (id, path)
+values ('gay-fish.gif', '/images/memes/gay-fish.gif');
+
+insert into meme (id)
+values ('GSxROquvfS.gif');
+insert into meme_tag (id, uuid, tag)
+values ('GSxROquvfS.gif', UUID(), 'south park');
+insert into meme_tag (id, uuid, tag)
+values ('GSxROquvfS.gif', UUID(), 'randy');
+insert into meme_tag (id, uuid, tag)
+values ('GSxROquvfS.gif', UUID(), 'oh hey fuck you');
+insert into meme_path (id, path)
+values ('GSxROquvfS.gif', '/images/memes/GSxROquvfS.gif');
+
+insert into meme (id)
+values ('bro-fist.gif');
+insert into meme_tag (id, uuid, tag)
+values ('bro-fist.gif', UUID(), 'final space');
+insert into meme_tag (id, uuid, tag)
+values ('bro-fist.gif', UUID(), 'bro fist');
+insert into meme_path (id, path)
+values ('bro-fist.gif', '/images/memes/bro-fist.gif');
+
+insert into meme (id)
+values ('source.gif');
+insert into meme_tag (id, uuid, tag)
+values ('source.gif', UUID(), 'south park');
+insert into meme_tag (id, uuid, tag)
+values ('source.gif', UUID(), 'kyle');
+insert into meme_tag (id, uuid, tag)
+values ('source.gif', UUID(), 'world of warcraft');
+insert into meme_tag (id, uuid, tag)
+values ('source.gif', UUID(), 'now we can finally play the game');
+insert into meme_path (id, path)
+values ('source.gif', '/images/memes/source.gif');
+
+insert into meme (id)
+values ('Xnec5Q.gif');
+insert into meme_tag (id, uuid, tag)
+values ('Xnec5Q.gif', UUID(), 'family guy');
+insert into meme_tag (id, uuid, tag)
+values ('Xnec5Q.gif', UUID(), 'star wars');
+insert into meme_tag (id, uuid, tag)
+values ('Xnec5Q.gif', UUID(), 'something something something dark side');
+insert into meme_path (id, path)
+values ('Xnec5Q.gif', '/images/memes/Xnec5Q.gif');
+
+insert into meme (id)
+values ('ge5vvm.jpeg');
+insert into meme_tag (id, uuid, tag)
+values ('ge5vvm.jpeg', UUID(), 'family guy');
+insert into meme_tag (id, uuid, tag)
+values ('ge5vvm.jpeg', UUID(), 'ostrich');
+insert into meme_tag (id, uuid, tag)
+values ('ge5vvm.jpeg', UUID(), 'ha haaa');
+insert into meme_path (id, path)
+values ('ge5vvm.jpeg', '/images/memes/ge5vvm.jpeg');
+
+insert into meme (id)
+values ('main-qimg-fd71cfc04709120de72285af475846a3-c.jpeg');
+insert into meme_tag (id, uuid, tag)
+values ('main-qimg-fd71cfc04709120de72285af475846a3-c.jpeg', UUID(), 'lordgasmic');
+insert into meme_tag (id, uuid, tag)
+values ('main-qimg-fd71cfc04709120de72285af475846a3-c.jpeg', UUID(), 'lord');
+insert into meme_tag (id, uuid, tag)
+values ('main-qimg-fd71cfc04709120de72285af475846a3-c.jpeg', UUID(), 'mortals');
+insert into meme_path (id, path)
+values ('main-qimg-fd71cfc04709120de72285af475846a3-c.jpeg',
+        '/images/memes/main-qimg-fd71cfc04709120de72285af475846a3-c.jpeg');
+
+insert into meme (id)
+values ('received_1392110004263102.jpeg');
+insert into meme_tag (id, uuid, tag)
+values ('received_1392110004263102.jpeg', UUID(), 'safe word');
+insert into meme_tag (id, uuid, tag)
+values ('received_1392110004263102.jpeg', UUID(), 'meatloaf');
+insert into meme_tag (id, uuid, tag)
+values ('received_1392110004263102.jpeg', UUID(), 'i won\'t do that');
+insert into meme_path (id, path)
+values ('received_1392110004263102.jpeg', '/images/memes/received_1392110004263102.jpeg');
+
+insert into meme (id)
+values ('3kcygo6cozq31.jpg');
+insert into meme_tag (id, uuid, tag)
+values ('3kcygo6cozq31.jpg', UUID(), 'freddie mercury');
+insert into meme_tag (id, uuid, tag)
+values ('3kcygo6cozq31.jpg', UUID(), 'sailor moon');
+insert into meme_path (id, path)
+values ('3kcygo6cozq31.jpg', '/images/memes/3kcygo6cozq31.jpg');
+
+insert into meme (id)
+values ('5f211e6.jpg');
+insert into meme_tag (id, uuid, tag)
+values ('5f211e6.jpg', UUID(), 'what is efficiency');
+insert into meme_tag (id, uuid, tag)
+values ('5f211e6.jpg', UUID(), 'clever laziness');
+insert into meme_path (id, path)
+values ('5f211e6.jpg', '/images/memes/5f211e6.jpg');
+
+insert into meme (id)
+values ('6aef1d3.jpg');
+insert into meme_tag (id, uuid, tag)
+values ('6aef1d3.jpg', UUID(), 'german wifi is the wurst');
+insert into meme_tag (id, uuid, tag)
+values ('6aef1d3.jpg', UUID(), 'internet');
+insert into meme_tag (id, uuid, tag)
+values ('6aef1d3.jpg', UUID(), 'computers');
+insert into meme_path (id, path)
+values ('6aef1d3.jpg', '/images/memes/6aef1d3.jpg');
+
+insert into meme (id)
+values ('6dcf863.jpg');
+insert into meme_tag (id, uuid, tag)
+values ('6dcf863.jpg', UUID(), 'fall expectations');
+insert into meme_tag (id, uuid, tag)
+values ('6dcf863.jpg', UUID(), 'internet');
+insert into meme_tag (id, uuid, tag)
+values ('6dcf863.jpg', UUID(), 'fall reality');
+insert into meme_path (id, path)
+values ('6dcf863.jpg', '/images/memes/6dcf863.jpg');
+
+insert into meme (id)
+values ('7f02bda.jpg');
+insert into meme_tag (id, uuid, tag)
+values ('7f02bda.jpg', UUID(), 'marvel');
+insert into meme_tag (id, uuid, tag)
+values ('7f02bda.jpg', UUID(), 'captain america');
+insert into meme_tag (id, uuid, tag)
+values ('7f02bda.jpg', UUID(), 'internet');
+insert into meme_tag (id, uuid, tag)
+values ('7f02bda.jpg', UUID(), 'computers');
+insert into meme_tag (id, uuid, tag)
+values ('7f02bda.jpg', UUID(), 'trump');
+insert into meme_path (id, path)
+values ('7f02bda.jpg', '/images/memes/7f02bda.jpg');
+
+insert into meme (id)
+values ('8-61b7037c5119b__700.jpg');
+insert into meme_tag (id, uuid, tag)
+values ('8-61b7037c5119b__700.jpg', UUID(), 'girl drink');
+insert into meme_tag (id, uuid, tag)
+values ('8-61b7037c5119b__700.jpg', UUID(), 'alcohol');
+insert into meme_tag (id, uuid, tag)
+values ('8-61b7037c5119b__700.jpg', UUID(), 'efficiency');
+insert into meme_tag (id, uuid, tag)
+values ('8-61b7037c5119b__700.jpg', UUID(), 'bread flavored soda');
+insert into meme_path (id, path)
+values ('8-61b7037c5119b__700.jpg', '/images/memes/8-61b7037c5119b__700.jpg');
+
+insert into meme (id)
+values ('58eedd0.jpg');
+insert into meme_tag (id, uuid, tag)
+values ('58eedd0.jpg', UUID(), 'cross stitch');
+insert into meme_tag (id, uuid, tag)
+values ('58eedd0.jpg', UUID(), 'eat a bag of dicks');
+insert into meme_path (id, path)
+values ('58eedd0.jpg', '/images/memes/58eedd0.jpg');
+
+insert into meme (id)
+values ('67f2fd1.jpg');
+insert into meme_tag (id, uuid, tag)
+values ('67f2fd1.jpg', UUID(), 'henry cavil');
+insert into meme_tag (id, uuid, tag)
+values ('67f2fd1.jpg', UUID(), 'witcher');
+insert into meme_tag (id, uuid, tag)
+values ('67f2fd1.jpg', UUID(), 'gwent');
+insert into meme_path (id, path)
+values ('67f2fd1.jpg', '/images/memes/67f2fd1.jpg');
+
+
+-- insert into meme (id) values ('aaaaaaaa');
+-- insert into meme_tag (id, uuid, tag) values ('aaaaaaa', UUID(), 'art');
+-- insert into meme_path (id, path) values ('aaaaaaa', '/images/memes/aaaaaaaa');
